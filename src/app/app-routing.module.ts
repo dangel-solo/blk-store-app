dc501e6ff4c97a484fd8a06f3bd722bfa31beeff
Dasb <dangel.solo@gmail.com>
2025-02-22 13:22:19 +0100

    folder structure and routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'stores',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'stores',
+        loadChildren: () =>
+          import('./features/stores/store.module').then((m) => m.StoreModule),
+      },
+    ],
+  },
+  { path: '**', redirectTo: 'stores', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
